5177a650668ed51f5e46637c365962918e6908e2
breachblockco <breachblockco@gmail.com>
2024-12-23 23:22:24 +0530

    express setup done

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log("Welcome to bookify")
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Listening on PORT: ${port}`);
+  });
+};
+
+
+startServer();
