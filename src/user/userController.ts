4c9ca1acdaef243ba1a7b94700718eeb51ea078f
breachblockco <breachblockco@gmail.com>
2024-12-25 11:58:43 +0530

    added validation to register fields and user model created.

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,9 +1,27 @@
-import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import userModel from "./userModel";
 
-const createUser= async (req:Request,res:Response,next:NextFunction)=>{
-    res.json({
-        message:"User created"
-    })
-}
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  // validation
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
 
-export {createUser}
+  // database call
+  const user = await userModel.findOne({ email });
+
+  if (user) {
+    const error = createHttpError(400, "User already exists with this email.");
+    return next(error);
+  }
+  // process
+  // response
+  res.json({
+    message: "User created",
+  });
+};
+
+export { createUser };
