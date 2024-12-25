79b2fbb9fb40f23c095a7b622da4c150b0c7f1d7
breachblockco <breachblockco@gmail.com>
2024-12-25 13:19:43 +0530

    added hashedpassword feature and configured jwt token on user registration.

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // validation
@@ -17,11 +20,23 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "User already exists with this email.");
     return next(error);
   }
-  // process
-  // response
-  res.json({
-    message: "User created",
+
+  // password hashing
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await userModel.create({
+    name,
+    email,
+    password:hashedPassword,
   });
+
+  // token generation JWT
+  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+  });
+
+  // response
+  res.json({ accessToken: token });
 };
 
 export { createUser };
