95a37db4d11478359c82fad743145974b65b6e62
breachblockco <breachblockco@gmail.com>
2024-12-29 01:16:37 +0530

    add footer

--- bookify-client-app/src/app/(home)/components/BookList.js
+++ bookify-client-app/src/app/(home)/components/BookList.js
@@ -3,7 +3,7 @@ import BookCard from './BookCard'
 
 function BookList({books}) {
   return (
-    <div className='grid grid-cols-1 gap-4 md:grid-cols-3 max-w-7xl mx-auto'>
+    <div className='grid grid-cols-1 gap-4 md:grid-cols-3 max-w-7xl mx-auto mb-14'>
         {
             books.map((book)=>(
                 <BookCard key={book._id} book={book}/>
---
+++ bookify-client-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="border-t p-5 text-center">
+      <span className='font-medium roboto-family text-lg'>© 2024 Bookify All rights reserved</span>
+    </footer>
+  );
+}
+
+export default Footer
