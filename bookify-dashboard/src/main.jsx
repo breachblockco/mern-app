a6809824751a8cfe36bce96e97cfe74bedc04e86
breachblockco <breachblockco@gmail.com>
2024-12-30 00:46:33 +0530

    add tanstack query

--- bookify-dashboard/src/main.jsx
+++ bookify-dashboard/src/main.jsx
@@ -3,7 +3,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router}/>
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
 );
--- bookify-dashboard/src/pages/LoginPage.jsx
+++ bookify-dashboard/src/pages/LoginPage.jsx
@@ -8,10 +8,23 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 
 function LoginPage() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault(); // Prevent form submission
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    // Log the data
+    console.log("data", { email, password });
+  };
+
   return (
     <div className="w-full h-screen flex items-center">
       <div className="w-1/4 mx-auto">
@@ -20,7 +33,7 @@ function LoginPage() {
             <CardTitle className="text-3xl">Login</CardTitle>
           </CardHeader>
           <CardContent>
-            <form>
+            <form onSubmit={handleLoginSubmit}>
               <div className="grid gap-6">
                 <div className="grid gap-6">
                   <div className="grid gap-2">
@@ -30,13 +43,20 @@ function LoginPage() {
                       type="email"
                       placeholder="m@example.com"
                       required
+                      ref={emailRef}
                     />
                   </div>
                   <div className="grid gap-2">
                     <div className="flex items-center">
                       <Label htmlFor="password">Password</Label>
                     </div>
-                    <Input id="password" type="password" required placeholder="Password" />
+                    <Input
+                      id="password"
+                      type="password"
+                      required
+                      placeholder="Password"
+                      ref={passwordRef}
+                    />
                   </div>
                   <Button type="submit" className="w-full">
                     Login
@@ -44,7 +64,10 @@ function LoginPage() {
                 </div>
                 <div className="text-center text-sm">
                   Don&apos;t have an account?{" "}
-                  <Link to={'/register'} className="underline underline-offset-4">
+                  <Link
+                    to={"/auth/register"}
+                    className="underline underline-offset-4"
+                  >
                     Register
                   </Link>
                 </div>
