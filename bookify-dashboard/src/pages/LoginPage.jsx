2029ce3ffa8b6972ae8c0cb86448af26f687f6be
breachblockco <breachblockco@gmail.com>
2024-12-29 15:50:28 +0530

    add login page

--- bookify-dashboard/src/pages/LoginPage.jsx
+++ bookify-dashboard/src/pages/LoginPage.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React from "react";
 
 function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <div className="w-full h-screen flex items-center">
+      <div className="w-1/4 mx-auto">
+        <Card>
+          <CardHeader className="text-center">
+            <CardTitle className="text-xl">Login</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid gap-6">
+                <div className="grid gap-6">
+                  <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      required
+                    />
+                  </div>
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Password</Label>
+                      <a
+                        href="#"
+                        className="ml-auto text-sm underline-offset-4 hover:underline"
+                      >
+                        Forgot your password?
+                      </a>
+                    </div>
+                    <Input id="password" type="password" required />
+                  </div>
+                  <Button type="submit" className="w-full">
+                    Login
+                  </Button>
+                </div>
+                <div className="text-center text-sm">
+                  Don&apos;t have an account?{" "}
+                  <a href="#" className="underline underline-offset-4">
+                    Sign up
+                  </a>
+                </div>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
