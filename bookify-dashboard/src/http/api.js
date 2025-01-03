83852abdbafee95c8a257aeb05e5b0e6b8e309e2
breachblockco <breachblockco@gmail.com>
2025-01-03 22:33:56 +0530

    add create book end point

--- bookify-dashboard/src/http/api.js
+++ bookify-dashboard/src/http/api.js
@@ -1,3 +1,4 @@
+import useTokenStore from "@/store";
 import axios from "axios";
 
 const api = axios.create({
@@ -7,7 +8,22 @@ const api = axios.create({
   },
 });
 
+api.interceptors.request.use((config)=>{
+  const token=useTokenStore.getState().token;
+  if(token){
+    config.headers.Authorization=`Bearer ${token}`
+  }
+
+  return config;
+})
+
 export const login = async (data) => api.post("/api/users/login", data);
 export const register = async (data) => api.post("/api/users/register", data);
 
 export const getBooks = async () => api.get("/api/books");
+
+export const createBook=async (data)=>api.post('/api/books',data,{
+  headers:{
+     'Content-Type':'multipart/form-data'
+  }
+})
--- bookify-dashboard/src/pages/CreateBook.jsx
+++ bookify-dashboard/src/pages/CreateBook.jsx
@@ -1,9 +1,221 @@
-import React from 'react'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { CirclePlus, LoaderCircle } from "lucide-react";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createBook } from "@/http/api";
+
+const formSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title must be atleast 2 characters.",
+  }),
+  genre: z.string().min(2, {
+    message: "Genre must be atleast 2 characters.",
+  }),
+  description: z.string().min(2, {
+    message: "Description must be atleast 2 characters.",
+  }),
+  coverImage: z.instanceof(FileList).refine((file) => {
+    return file.length === 1;
+  }, "Cover Image is required"),
+  file: z.instanceof(FileList).refine((file) => {
+    return file.length === 1;
+  }, "Book PDF is required"),
+});
 
 function CreateBook() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      genre: "",
+      description: "",
+    },
+  });
+
+  const coverImageRef = form.register("coverImage");
+  const fileRef = form.register("file");
+
+  const queryClient = useQueryClient();
+
+  const navigate = useNavigate();
+
+  const mutation = useMutation({
+    mutationFn: createBook,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["books"] });
+      navigate("/dashboard/books");
+    },
+  });
+
+  function onSubmit(values) {
+    const formdata = new FormData();
+    formdata.append("title", values.title);
+    formdata.append("genre", values.genre);
+    formdata.append("description", values.description);
+    formdata.append("coverImage", values.coverImage[0]);
+    formdata.append("file", values.file[0]);
+    mutation.mutate(formdata);
+  }
   return (
-    <div>CreateBook</div>
-  )
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="flex items-center justify-between">
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbLink href="/dashboard/home">Home</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbLink href="/dashboard/books">Books</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Create</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+            <div className="flex gap-3 items-center">
+              <Link to={'/dashboard/books'}>
+                <Button variant="outline">
+                  <span className="ml-2">Cancel</span>
+                </Button>
+              </Link>
+              <Button
+                type="submit"
+                disabled={mutation.isPending}
+                className="w-full flex gap-2"
+              >
+                <span>
+                  {mutation.isPending && (
+                    <LoaderCircle className={`animate-spin`} />
+                  )}
+                </span>
+                <span>Submit</span>
+              </Button>
+            </div>
+          </div>
+          <Card className="mt-6">
+            <CardHeader>
+              <CardTitle>Create a new book</CardTitle>
+              <CardDescription>
+                Fill out the form below to create a new book.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid gap-6">
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <Input type="text" className="w-full" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="genre"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Genre</FormLabel>
+                      <FormControl>
+                        <Input type="text" className="w-full" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="coverImage"
+                  render={() => (
+                    <FormItem>
+                      <FormLabel>Cover Image</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="file"
+                          className="w-full"
+                          {...coverImageRef}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="file"
+                  render={() => (
+                    <FormItem>
+                      <FormLabel>Book File</FormLabel>
+                      <FormControl>
+                        <Input type="file" className="w-full" {...fileRef} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          id="description"
+                          className="min-h-32"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </form>
+      </Form>
+    </div>
+  );
 }
 
-export default CreateBook
+export default CreateBook;
